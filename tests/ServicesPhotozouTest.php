cc3ad9ccd6fd4f345e8b22742aa5353bc3a9ab14
ha1t <halt.feits@gmail.com>
2012-04-26 18:59:01 +0900

    photo_list_publicのテストを移行

--- tests/ServicesPhotozouTest.php
+++ tests/ServicesPhotozouTest.php
@@ -3,12 +3,32 @@ require_once 'Services/Photozou.php';
 
 class ServicesPhotozouTest extends PHPUnit_Framework_TestCase
 {
-    public function testPhotoAlbum()
+    public function setUp()
     {
         global $user;
         global $password;
-        $photozou = new Services_Photozou($user, $password);
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public function testPhotoAlbum()
+    {
+        $photozou = new Services_Photozou($this->user, $this->password);
         $this->assertTrue(is_array($photozou->photo_album()));
+    }
+
+    public function testPhotoListPublic()
+    {
+        $photozou = new Services_Photozou($this->user, $this->password);
+
+        $result = $photozou->photo_list_public(
+            array(
+                'type' => 'public',
+                'user_id' => '44520',
+                'limit' => '2'
+            )
+        );
+        $this->assertTrue(is_array($result));
     }
 }
 
